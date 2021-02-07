dbc237cbd8eadbdbdc7326346718a3562096ffef
juan.cahuana <juan.cahuana@globant.com>
2021-02-07 01:02:52 -0500

    Add es6 templates

---
+++ typescript/literal-templates.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    var hola = "hola";
+    var name = "Juan";
+    var surname = "Cahuana";
+    var message = "Saludo:\n  Hola " + name + " " + surname;
+    console.log(message);
+})();
---
+++ typescript/literal-templates.ts
@@ -0,0 +1,11 @@
+(() => {
+  var hola = "hola";
+
+  const name = "Juan";
+  const surname = "Cahuana";
+
+  const message = `Saludo:
+  Hola ${name} ${surname}`;
+
+  console.log(message);
+})();
