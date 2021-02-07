6dc2efb1bcca95cc87c75f5b26a139efbca0dba1
juan.cahuana <juan.cahuana@globant.com>
2021-02-07 00:47:35 -0500

    Add exclude directories

---
+++ typescript/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function hello(name) {
+    var message = "Hola, " + name;
+    return message;
+}
+var helloString = hello("Juan Aantonio");
+console.log(helloString);
---
+++ typescript/app.ts
@@ -0,0 +1,6 @@
+function hello(name: string): string {
+  const message = "Hola, " + name;
+  return message;
+}
+const helloString = hello("Juan Aantonio");
+console.log(helloString);
