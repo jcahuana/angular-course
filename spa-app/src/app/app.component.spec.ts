345dcbbe0b0d16fbabaebf452cb1c272ed6f3e95
Juan Cahuana <jcahuanam@gmail.com>
2021-02-10 01:09:07 -0500

    Change project name

--- spa-app/src/app/app.component.spec.ts
+++ spa-app/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spsa-app'`, () => {
+  it(`should have as title 'spa-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spsa-app');
+    expect(app.title).toEqual('spa-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('spsa-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'spa-app app is running!'
+    );
   });
 });
